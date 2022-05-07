256da95b5b695641f8e252c13fc68398574bedb8
Gaston <corbalan.gaston@outlook.com>
2022-05-07 13:55:05 -0300

    css

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ const Home = () => {
     return (
     <div>
         <h1>Gaston Corbalan</h1>
-        <p> Hi! <span>👋</span>, I'm Gastón a frontend dev based in Buenos Aires. I’ve spent the past 10+ years been a Bussiness owner and now I'm looking to make new digital products
-            .</p>
+        <p> Hi! <span>👋</span>, I'm Gastón a frontend dev based in Buenos Aires. I’ve spent the past 10+ years been a Bussiness owner and now I'm looking to make new digital products.</p>
     </div>
     )
 }
