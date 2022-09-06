5ed8b09373742761374c073553a77ca616bcc61e
Gaston <corbalan.gaston@outlook.com>
2022-09-06 11:20:52 -0300

    open graph images

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,18 @@
-import '../styles/styles.css'
-import Head from 'next/head'
-import Layout from '../components/Layout/Layout'
+import '../styles/styles.css';
+import Head from 'next/head';
+import Layout from '../components/Layout/Layout';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
-            <title>Gastón Corbalán</title>
-            <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏽‍💻</text></svg>"></link>
+                <title>Gastón Corbalán</title>
+                <link rel="icon" href="/favicon.png"></link>
             </Head>
             <Layout>
                 <Component {...pageProps} />
             </Layout>
         </>
-    )
+    );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,59 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
     render() {
-    return (
-        <Html>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <link href="https://fonts.googleapis.com/css2?family=Quattrocento:wght@400;700&family=Raleway:wght@300;400&display=swap" rel="stylesheet" />
-            </Head>
-        <body>
-            <Main />
-            <NextScript />
-        </body>
-        </Html>
-        )
+        return (
+            <Html>
+                <Head>
+                    <meta
+                        name="description"
+                        content="Gastón Corbalán works and resume"
+                    />
+
+                    <meta
+                        property="og:url"
+                        content="https://www.gastoncorbalan.com/"
+                    />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:title" content="Gastón Corbalán" />
+                    <meta
+                        property="og:description"
+                        content="Gastón Corbalán Portfolio"
+                    />
+                    <meta property="og:image" content="/opengraph-cover.png" />
+
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta
+                        property="twitter:domain"
+                        content="gastoncorbalan.com"
+                    />
+                    <meta
+                        property="twitter:url"
+                        content="https://www.gastoncorbalan.com/"
+                    />
+                    <meta name="twitter:title" content="Gastón Corbalán" />
+                    <meta
+                        name="twitter:description"
+                        content="Gastón Corbalán Portfolio"
+                    />
+                    <meta name="twitter:image" content="/opengraph-cover.png" />
+                    <link
+                        rel="preconnect"
+                        href="https://fonts.googleapis.com"
+                    />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Quattrocento:wght@400;700&family=Raleway:wght@300;400&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
 
-export default MyDocument
+export default MyDocument;
