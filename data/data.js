a0bf75b27ff8bbcc0a82a400562def961ae83bfd
Gaston <corbalan.gaston@outlook.com>
2022-10-05 10:49:15 -0300

    links update

--- data/data.js
+++ data/data.js
@@ -1,58 +1,58 @@
-import * as icon  from 'react-icons/si'
+import * as icon from 'react-icons/si';
 
 const work = [
-	{
-		id: '1',
-		name: 'Todo app',
-		description: `A simple, must-do, To-do app. Am I really a junior developer if I don't have one?.`,
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://to-do-platzi.vercel.app/',
-		linkGithub: 'https://github.com/ImGaston/to-do-list',
-	},
-	{
-		id: '2',
-		name: 'Quote-generator',
-		description: 'Random quote generator from Leo Piccioli Linkedin articles.',
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://imgaston.github.io/quote-generator/',
-		linkGithub: 'https://github.com/ImGaston/quote-generator',
-	},
-	{
-		id: '3',
-		name: 'Pharmacys on duty',
-		description: 'A solution to know which pharmacy is open in my hometown.',
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://farmacias.gastoncorbalan.com',
-		linkGithub: 'https://github.com/ImGaston/farmacias-de-turno',
-	},
-	{
-		id: '4',
-		name: 'Cards play note',
-		description: 'A solution to an argentian game called Truco.',
-		stack: [
-			{ icon: icon.SiHtml5, name: 'HTML' },
-			{ icon: icon.SiCss3, name: 'CSS' },
-			{ icon: icon.SiJavascript, name: 'Javascript' },
-			{ icon: icon.SiReact, name: 'ReactJS' },
-		],
-		linkProject: 'https://anotador.com.ar/',
-		linkGithub: 'https://github.com/ImGaston/anotador',
-	},
-]
+  {
+    id: '1',
+    name: 'Todo app',
+    description: `A simple, must-do, To-do app. Am I really a junior developer if I don't have one?.`,
+    stack: [
+      { icon: icon.SiHtml5, name: 'HTML' },
+      { icon: icon.SiCss3, name: 'CSS' },
+      { icon: icon.SiJavascript, name: 'Javascript' },
+      { icon: icon.SiReact, name: 'ReactJS' },
+    ],
+    linkProject: 'https://todo-list.imgaston.com/',
+    linkGithub: 'https://github.com/ImGaston/to-do-list',
+  },
+  {
+    id: '2',
+    name: 'Quote-generator',
+    description: 'Random quote generator from Leo Piccioli Linkedin articles.',
+    stack: [
+      { icon: icon.SiHtml5, name: 'HTML' },
+      { icon: icon.SiCss3, name: 'CSS' },
+      { icon: icon.SiJavascript, name: 'Javascript' },
+      { icon: icon.SiReact, name: 'ReactJS' },
+    ],
+    linkProject: 'https://imgaston.github.io/quote-generator/',
+    linkGithub: 'https://github.com/ImGaston/quote-generator',
+  },
+  {
+    id: '3',
+    name: 'Pharmacys on duty',
+    description: 'A solution to know which pharmacy is open in my hometown.',
+    stack: [
+      { icon: icon.SiHtml5, name: 'HTML' },
+      { icon: icon.SiCss3, name: 'CSS' },
+      { icon: icon.SiJavascript, name: 'Javascript' },
+      { icon: icon.SiReact, name: 'ReactJS' },
+    ],
+    linkProject: 'https://farmacias.imgaston.com',
+    linkGithub: 'https://github.com/ImGaston/farmacias-de-turno',
+  },
+  {
+    id: '4',
+    name: 'Cards play note',
+    description: 'A solution to an argentian game called Truco.',
+    stack: [
+      { icon: icon.SiHtml5, name: 'HTML' },
+      { icon: icon.SiCss3, name: 'CSS' },
+      { icon: icon.SiJavascript, name: 'Javascript' },
+      { icon: icon.SiReact, name: 'ReactJS' },
+    ],
+    linkProject: 'https://anotador.com.ar/',
+    linkGithub: 'https://github.com/ImGaston/anotador',
+  },
+];
 
 export default work;
