82c62abf98bb8c3a7d2605f5b339c80a2b2d6ec2
Gaston <corbalan.gaston@outlook.com>
2024-02-23 09:07:36 +0100

    new profile

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import ProjectList from '../components/ProjectList/ProjectList';
 import StackList from '../components/StackList/StackList';
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <main>
       <div className='first-view'>
         <h1>Gastón Corbalán</h1>
+      </div>
+      <div>
+        <a target='_blank' href='https://notes.gastoncorbalan.com/'>
+          Notes
+        </a>
+      </div>
+      <div>
+        <a target='_blank' href='https://cv.gastoncorbalan.com/'>
+          CV
+        </a>
       </div>
     </main>
   );
